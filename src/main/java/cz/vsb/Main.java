7b8971bd463779493a5bee25dea158bd2ab88602
HEN0047 <JHendl993@gmail.com>
2016-10-05 17:08:16 +0200

    init

---
+++ src/main/java/cz/vsb/Main.java
@@ -0,0 +1,24 @@
+package cz.vsb;
+
+
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.boot.builder.SpringApplicationBuilder;
+        import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * @author ruz76
+ */
+@SpringBootApplication(scanBasePackages = {"cz.vsb"})
+public class Main extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Main.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
